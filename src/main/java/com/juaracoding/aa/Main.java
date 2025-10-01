2e160a3535fc5418762692c7214e5f6174021baf
sourchib <muchammadmuchib@gmail.com>
2025-10-01 21:39:35 +0700

    day 2 topik type data

---
+++ src/main/java/com/juaracoding/aa/Main.java
@@ -0,0 +1,13 @@
+package com.juaracoding.aa;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int x;
+        int z;
+        int y;
+
+        System.out.println("Hello World 1");
+        System.out.println(Integer.toBinaryString(72));//1001000
+    }//COMPILER
+}//INTERPRETER
---
+++ src/main/java/com/juaracoding/aa/Main1.java
@@ -0,0 +1,14 @@
+package com.juaracoding.aa;
+
+public class Main1 {
+}
+
+/*
+IntelliJ IDEA 2022.3.1 (Community Edition)
+Build #IC-223.8214.52, built on December 20, 2022
+@Author LENOVO a.k.a. Paul Christian
+Java Developer
+Created on 01/10/2025 20:18
+@Last Modified 01/10/2025 20:18
+Version 1.0
+*/
